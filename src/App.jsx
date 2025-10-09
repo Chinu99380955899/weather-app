46323998e4db3f1c3a2e0acbc5fef94bec202534
chinmaya_sutar <sutarchinmaya385@gmail.com>
2025-10-09 13:08:17 +0530

    chinu changed a file

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import "./App.css";
 import WetherApp from "./assets/WeatherApp";
 
 function App() {
-  let handelClick = () => {
+  let handelClick = () => {     
     console.log("buton wAS clicked");
   };
 //i am changeing sonthing
 //i am sunny i change somthing
-//sunny changes at 12.38
+//chinu changes at 12.44
   return (
     <>
       <WetherApp />
